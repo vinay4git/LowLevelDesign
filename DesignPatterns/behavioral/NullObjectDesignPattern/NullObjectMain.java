254c8301f930c0f198a05b8da56ba9574e874408
Vinay Kumar Bandlamudi - b0k03k3 <bandlamudi.kumar@walmart.com>
2025-04-12 00:21:54 +0530

    Null Object Pattern

--- DesignPatterns/behavioral/NullObjectDesignPattern/NullObjectMain.java
+++ DesignPatterns/behavioral/NullObjectDesignPattern/NullObjectMain.java
@@ -1,7 +1,6 @@
 package DesignPatterns.behavioral.NullObjectDesignPattern;
 
 public class NullObjectMain {
-
     public static void main(String[] args) {
         System.out.println(new VehicleFactory().getVehicle("Car").getModel());
         System.out.println(new VehicleFactory().getVehicle("Bicycle").getModel());
