ccdb0a682ab0f935f724f9b8860c13e999ea5198
Vinay Kumar Bandlamudi - b0k03k3 <bandlamudi.kumar@walmart.com>
2025-04-19 00:26:57 +0530

    take inpu

--- DesignPatterns/structural/D_AdapterPattern/AdapterMain.java
+++ DesignPatterns/structural/D_AdapterPattern/AdapterMain.java
@@ -3,9 +3,13 @@ package DesignPatterns.structural.D_AdapterPattern;
 import DesignPatterns.structural.D_AdapterPattern.Adaptee.WeighingMachineForBabies;
 import DesignPatterns.structural.D_AdapterPattern.Adapter.WeightPoundToKgAdapterImpl;
 
+import java.util.Scanner;
+
 public class AdapterMain {
     public static void main(String[] args) {
-        WeighingMachineForBabies weighingMachineForBabies = new WeighingMachineForBabies(30);
+        Scanner s = new Scanner(System.in);
+        int weight = s.nextInt();
+        WeighingMachineForBabies weighingMachineForBabies = new WeighingMachineForBabies(weight);
         WeightPoundToKgAdapterImpl weightPoundToKgAdapter = new WeightPoundToKgAdapterImpl(weighingMachineForBabies);
 
         System.out.println(weightPoundToKgAdapter.getWeightInKg());
