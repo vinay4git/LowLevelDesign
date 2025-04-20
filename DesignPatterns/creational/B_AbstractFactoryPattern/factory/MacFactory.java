ccdc6d05332a03d3e23daede156822bc29c021c8
Vinay Kumar Bandlamudi - b0k03k3 <bandlamudi.kumar@walmart.com>
2025-04-20 13:12:41 +0530

    abastract factory corrections

--- DesignPatterns/creational/B_AbstractFactoryPattern/factory/MacFactory.java
+++ DesignPatterns/creational/B_AbstractFactoryPattern/factory/MacFactory.java
@@ -6,13 +6,16 @@ import DesignPatterns.creational.B_AbstractFactoryPattern.models.dialog.Dialog;
 import DesignPatterns.creational.B_AbstractFactoryPattern.models.dialog.MacDialog;
 
 public class MacFactory implements UIFactory{
+    Button macButton = new MacButton();
+    Dialog macDialog = new MacDialog();
+
     @Override
     public Button createButton() {
-        return new MacButton();
+        return macButton;
     }
 
     @Override
     public Dialog createDialog() {
-        return new MacDialog();
+        return macDialog;
     }
 }
--- DesignPatterns/creational/B_AbstractFactoryPattern/factory/WindowsFactory.java
+++ DesignPatterns/creational/B_AbstractFactoryPattern/factory/WindowsFactory.java
@@ -6,13 +6,15 @@ import DesignPatterns.creational.B_AbstractFactoryPattern.models.dialog.Dialog;
 import DesignPatterns.creational.B_AbstractFactoryPattern.models.dialog.WindowsDialog;
 
 public class WindowsFactory implements UIFactory{
+    Button windowsButton = new WindowsButton();
+    Dialog windowsDialog = new WindowsDialog();
     @Override
     public Button createButton() {
-        return new WindowsButton();
+        return windowsButton;
     }
 
     @Override
     public Dialog createDialog() {
-        return new WindowsDialog();
+        return windowsDialog;
     }
 }
